366768c7323360267fe791031219c5fef8427f55
Will Richards <spectralsounddesign@gmail.com>
2022-09-10 13:29:02 -0400

    created ui

--- main.js
+++ main.js
@@ -2,16 +2,46 @@ const firstNumber = document.getElementById("first-number");
 const secondNumber = document.getElementById("second-number");
 const textOut = document.getElementsByClassName("text-out");
 
-const add = document.getElementById("add");
+/*const add = document.getElementById("add");
 const subtract = document.getElementById("subtract");
 const multiply = document.getElementById("multiply");
-const divide = document.getElementById("divide");
+const divide = document.getElementById("divide");*/
 
 
-function addNum(firstNumber, secondNumber) {
-    textOut.innerHTML = firstNumber + secondNumber;
-    console.log(firstNumber);
+function add(firstNumber, secondNumber) {
+    const total = firstNumber + secondNumber;
+    console.log(total);
+
+}
+function subtract(firstNumber, secondNumber) {
+    const total = firstNumber - secondNumber;
+    console.log(total);
+
+}
+function multiply(firstNumber, secondNumber) {
+    const total = firstNumber * secondNumber;
+    console.log(total);
+
+}
+function divide(firstNumber, secondNumber) {
+    const total = firstNumber / secondNumber;
+    console.log(total);
 
 }
 
-add.addEventListener("click", addNum);
+function operate(op, firstNumber, secondNumber) {
+    if (op == "+") {
+        add(firstNumber, secondNumber);
+    }
+    else if (op == "-") {
+        subtract(firstNumber, secondNumber);
+    }
+    else if (op == "*") {
+        multiply(firstNumber, secondNumber);
+    }
+    else if (op == "/") {
+        divide(firstNumber, secondNumber);
+    }
+
+}
+
