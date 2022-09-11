12a41f1bc878121916e50fadceb062d1543492c9
Will Richards <spectralsounddesign@gmail.com>
2022-09-11 17:56:27 -0400

    created array to populate with display data

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const disp = document.querySelector("#display");
 const numBtns = document.querySelectorAll(".num-btns");
 const opBtns = document.querySelectorAll(".op-btns");
 
-const input = [];
+let firstNumber = ""
+let input = [];
 
 /*const add = document.getElementById("add");
 const subtract = document.getElementById("subtract");
@@ -12,37 +13,41 @@ const divide = document.getElementById("divide");*/
 
 function add(firstNumber, secondNumber) {
     const total = firstNumber + secondNumber;
+    return total
     console.log(total);
 
 }
 function subtract(firstNumber, secondNumber) {
     const total = firstNumber - secondNumber;
+    return total
     console.log(total);
 
 }
 function multiply(firstNumber, secondNumber) {
     const total = firstNumber * secondNumber;
+    return total
     console.log(total);
 
 }
 function divide(firstNumber, secondNumber) {
     const total = firstNumber / secondNumber;
+    return total
     console.log(total);
 
 }
 
 function operate(op, firstNumber, secondNumber) {
     if (op == "+") {
-        add(firstNumber, secondNumber);
+        return add(firstNumber, secondNumber);
     }
     else if (op == "-") {
-        subtract(firstNumber, secondNumber);
+        return subtract(firstNumber, secondNumber);
     }
     else if (op == "*") {
-        multiply(firstNumber, secondNumber);
+        return multiply(firstNumber, secondNumber);
     }
     else if (op == "/") {
-        divide(firstNumber, secondNumber);
+        return divide(firstNumber, secondNumber);
     }
 
 }
@@ -50,11 +55,25 @@ function operate(op, firstNumber, secondNumber) {
 //display numbers
 function display(num) {
     disp.textContent += num;
-    if (num == "+") {
-        input.push(disp.textContent);
-        disp.textContent = "";
-        console.log(input);
-    }
+}
+
+function calculate(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[1] == "+" || arr[1] == "-" || arr[1] == "*" || arr[1] == "/") {
+            total += operate(arr[i], parseInt(arr[i-1]), parseInt(arr[i+1]));
+            continue;
+        }
+        else if (arr[i] == "+" || arr[i] == "-" || arr[i] == "*" || arr[i] == "/"){
+            total = operate(arr[i], total, parseInt(arr[i+1]));
+        }
+        else if (arr[i] == "=") {
+            return total;
+        }
+           
+        
+    } 
+    
 }
 
 //get input from number buttons
@@ -67,7 +86,19 @@ numBtns.forEach((button) => {
 //get input from operator buttons
 opBtns.forEach((button) => {
     button.addEventListener('click', () => {
-        display(button.innerHTML);
+        //if op is hit display previous num + op
+        input.push(disp.textContent);
+        input.push(button.textContent);
+        console.log(input);
+        disp.textContent = "";
+        if (button.textContent == "=") {
+            console.log(calculate(input));
+        }
+        else if (button.textContent == "clear") {
+            disp.textContent == "";
+            input = [];
+            console.log(input);
+        }
     });
 });
 
