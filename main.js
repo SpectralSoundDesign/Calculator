faf54f37d2ac3edc441907f70900ca0843b02582
Will Richards <spectralsounddesign@gmail.com>
2022-09-15 10:56:46 -0400

    reworked calculator functionality

--- main.js
+++ main.js
@@ -1,105 +1,88 @@
 const disp = document.querySelector("#display");
 const numBtns = document.querySelectorAll(".num-btns");
 const opBtns = document.querySelectorAll(".op-btns");
+const equalBtn = document.querySelector(".equals-btn");
+const clearBtn = document.querySelector(".clear-btn");
 
-let firstNumber = ""
-let input = [];
+let operation = '';
+let previousOperand = '';
 
-/*const add = document.getElementById("add");
-const subtract = document.getElementById("subtract");
-const multiply = document.getElementById("multiply");
-const divide = document.getElementById("divide");*/
-
-
-function add(firstNumber, secondNumber) {
-    const total = firstNumber + secondNumber;
-    return total
-    console.log(total);
-
-}
-function subtract(firstNumber, secondNumber) {
-    const total = firstNumber - secondNumber;
-    return total
-    console.log(total);
-
-}
-function multiply(firstNumber, secondNumber) {
-    const total = firstNumber * secondNumber;
-    return total
-    console.log(total);
-
-}
-function divide(firstNumber, secondNumber) {
-    const total = firstNumber / secondNumber;
-    return total
-    console.log(total);
-
+function clear() {
+    disp.textContent = '';
+    operation = undefined;
+    previousOperand = '';
 }
 
-function operate(op, firstNumber, secondNumber) {
-    if (op == "+") {
-        return add(firstNumber, secondNumber);
-    }
-    else if (op == "-") {
-        return subtract(firstNumber, secondNumber);
-    }
-    else if (op == "*") {
-        return multiply(firstNumber, secondNumber);
-    }
-    else if (op == "/") {
-        return divide(firstNumber, secondNumber);
-    }
-
-}
-
-//display numbers
-function display(num) {
+function displayNum(num) {
     disp.textContent += num;
 }
 
-function calculate(arr) {
-    let total = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[1] == "+" || arr[1] == "-" || arr[1] == "*" || arr[1] == "/") {
-            total += operate(arr[i], parseInt(arr[i-1]), parseInt(arr[i+1]));
-            continue;
-        }
-        else if (arr[i] == "+" || arr[i] == "-" || arr[i] == "*" || arr[i] == "/"){
-            total = operate(arr[i], total, parseInt(arr[i+1]));
-        }
-        else if (arr[i] == "=") {
-            return total;
-        }
-           
-        
-    } 
-    
+function chooseOperation(op) {
+    if (disp.textContent === '') {
+        return;
+    }
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = disp.textContent;
+    disp.textContent = '';
 }
 
-//get input from number buttons
+function compute() {
+    let total;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(disp.textContent);
+    if (isNaN(prev) || isNaN(current)) {
+        return;
+    }
+
+    switch (operation) {
+        case '+':
+            total = prev + current;
+            break;
+        case '-':
+            total = prev - current;
+            break;
+        case '*':
+            total = prev * current;
+            break;
+        case '/':
+            if (current === 0) {
+                disp.textContent = "Why???";
+                return;
+            }
+            total = prev / current;
+            break;
+        default:
+            return;
+    }
+
+    disp.textContent = total;
+    operation = undefined;
+    previousOperand = '';
+
+
+}
+
 numBtns.forEach((button) => {
     button.addEventListener('click', () => {
-        display(button.innerHTML);
+        displayNum(button.textContent);
     });
 });
 
-//get input from operator buttons
 opBtns.forEach((button) => {
     button.addEventListener('click', () => {
-        //if op is hit display previous num + op
-        input.push(disp.textContent);
-        input.push(button.textContent);
-        console.log(input);
-        disp.textContent = "";
-        if (button.textContent == "=") {
-            console.log(calculate(input));
-        }
-        else if (button.textContent == "clear") {
-            disp.textContent == "";
-            input = [];
-            console.log(input);
-        }
+        chooseOperation(button.textContent);
     });
+});
+
+equalBtn.addEventListener('click', () => {
+    compute();
+});
+
+clearBtn.addEventListener('click', () => {
+    clear();
 });
 
 
